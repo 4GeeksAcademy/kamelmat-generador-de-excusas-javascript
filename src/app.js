a893b2dcf125882f5fb7d43426650c501b9227e6
kamelmat <125071787+kamelmat@users.noreply.github.com>
2024-02-08 16:45:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,59 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "My", "Your", "The"];
+  let subject = [
+    "alien",
+    "cat",
+    "dog",
+    "truck",
+    "grandpa",
+    "CIA agent",
+    "bystander",
+    "sister",
+    "band-mate"
+  ];
+  let action = [
+    "took my",
+    "ate my",
+    "thrashed my",
+    "flushed my",
+    "lost my",
+    "stole my",
+    "threw my"
+  ];
+  let posession = ["car", "guitar", "homework", "hand", "keys"];
+  let where = [
+    "in my backyard",
+    "in my garage",
+    "on the street",
+    "in the park"
+  ];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let possIndx = Math.floor(Math.random() * posession.length);
+  let wheIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    posession[possIndx] +
+    " " +
+    where[wheIndx]
+  );
 };
